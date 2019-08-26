edeae2f9ad78ef781690cb49132e939c750edfa4
Pedro Henrique Souza De Queiroz <38128442813@info.local>
2019-08-26 17:26:55 -0300

    UPZADA

---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Controller/UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Domains;
+using Senai.Optus.WebApi.Repositories;
+
+namespace Senai.Optus.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        UsuarioRepository usuarioRepository = new UsuarioRepository();
+        
+        [HttpGet]
+        public IActionResult Listar(Usuarios usuario)
+        {
+            return Ok(usuarioRepository.Listar());
+        }
+        [HttpPost]
+        public IActionResult Cadastrar (Usuarios usuario)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Senai.Optus.WebApi/Senai.Optus.WebApi/Repositories/UsuarioRepository.cs
@@ -0,0 +1,48 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class UsuarioRepository
+    {
+        public List<Usuarios> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Usuarios.ToList();
+            }
+        }
+        public void Cadastrar(Usuarios Usuarios)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                ctx.Usuarios.Add(Usuarios);
+                ctx.SaveChanges();
+            }
+        }
+
+        public Usuarios BuscarPorId(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Usuarios.FirstOrDefault(x => x.IdUsuario == id);
+            }
+        }
+        public void Deletar(int id)
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                Usuarios Usuario = ctx.Usuarios.Find(id);
+
+                ctx.Remove(Usuario);
+
+                ctx.SaveChanges();
+            }
+        }
+
+
+    }
+}
